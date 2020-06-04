32bec913d9bfec989a2ecd342de81876b6d3e0b3
zhuhangjie <zhuhangjietiancai@foxmail.com>
2020-06-04 23:31:21 +0800

    add BST init function

--- src/com/zhuhangjie/datastructure/tree/BinarySearchTree.java
+++ src/com/zhuhangjie/datastructure/tree/BinarySearchTree.java
@@ -20,4 +20,20 @@ public class BinarySearchTree<E extends Comparable> {
       this.right = null;
     }
   }
+
+  private Node root;
+  private int size;
+
+  public BinarySearchTree() {
+    this.root = null;
+    this.size = 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean isEmpty(){
+    return size == 0;
+  }
 }
