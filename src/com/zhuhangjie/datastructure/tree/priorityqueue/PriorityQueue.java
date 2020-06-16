d82a9af5453fc2e8e6cd587d1358962e7c751f2c
zhuhangjie <772959377@qq.com>
2020-06-16 16:38:54 +0800

    添加注解

--- src/com/zhuhangjie/datastructure/tree/priorityqueue/PriorityQueue.java
+++ src/com/zhuhangjie/datastructure/tree/priorityqueue/PriorityQueue.java
@@ -7,6 +7,8 @@ import com.zhuhangjie.datastructure.queue.Queue;
  * 1.普通数组来做优先队列，enqueue为O(1),dequeue为O(n)
  * 2.顺序数组来做优先队列，enqueue为O(n),dequeue为O(1)
  * 3.最大堆来做有限队列,enqueue为O(logn),dequeue为O(logn)
+ *
+ * Java的PriorityQueue是最小堆
  * @param <E>
  */
 public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
